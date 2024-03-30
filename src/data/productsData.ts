704552055f2ce6e38201bbe8ee175bc185570116
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2024-03-30 21:38:22 +0100

    refactor: delete data folder from src folder root level

--- src/data/productsData.ts
+++
@@ -1,5 +0,0 @@
-export default function getProducts(): Promise<Product[]> {
-  return fetch("https://dummyjson.com/products?limit=10")
-    .then((res) => res.json())
-    .then((res) => res.products);
-}
