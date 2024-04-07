4205001472ded34b688bfbd298245e1f5f276142
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2024-04-07 16:34:42 +0200

    feat: use suspense for product detail page

--- src/app/product-detail/[id]/page.tsx
+++ src/app/product-detail/[id]/page.tsx
@@ -1,12 +1,6 @@
-import React from "react";
-import Container from "@mui/material/Container";
-import AddToCartButton from "@/components/addToCartButton/AddToCartButton";
-import { notFound } from "next/navigation";
-import { getProduct } from "../actions/productDetailAction";
-import { Box, Stack, Typography } from "@mui/material";
-import ProductImages from "@/components/productImages/ProductImages";
-import BackToButton from "@/components/backToButton/BackToButton";
-import Price from "@/components/price/Price";
+import React, { Suspense } from "react";
+import Loading from "./loading";
+import Product from "@/components/product/Product";
 
 interface ProductDetailPageProps {
   params: {
@@ -15,32 +9,10 @@ interface ProductDetailPageProps {
 }
 
 async function ProductDetailPage({ params }: ProductDetailPageProps) {
-  const product = await getProduct(params.id);
-
-  if (!product) {
-    return notFound();
-  }
-
   return (
-    <Container>
-      <Stack>
-        <Box>
-          <BackToButton target="/product-list" text="Back to products" />
-          <Box>
-            <ProductImages images={product.images} />
-          </Box>
-          <Stack marginTop="10px">
-            <Box>
-              <Typography variant="body1">{product.title}</Typography>
-              <Price price={product.price} />
-            </Box>
-            <Box>
-              <AddToCartButton product={product} />
-            </Box>
-          </Stack>
-        </Box>
-      </Stack>
-    </Container>
+    <Suspense key={params.id} fallback={<Loading />}>
+      <Product id={params.id} />
+    </Suspense>
   );
 }
 
--- src/app/product-detail/actions/productDetailAction.tsx
+++ src/app/product-detail/actions/productDetailAction.tsx
@@ -1,10 +1,6 @@
 import { getProductsFromDB, getProductFromDB } from "@/db/queries/product";
 import { IProduct } from "@/lib/types";
 
-export async function getProducts(query?: string): Promise<IProduct[]> {
-  return query ? await getProductsFromDB(query) : await getProductsFromDB();
-}
-
 export async function getProduct(id: string): Promise<IProduct> {
   return await getProductFromDB(id);
 }
