b5a1cafa506a7a3369f08f75f083fdfa6631f542
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2024-04-09 23:41:21 +0200

    fix: layout shift after landing

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,11 +1,14 @@
 import CartContent from "@/components/cartContent/CartContent";
 import { Suspense } from "react";
 import Loading from "../loading";
+import { Container } from "@mui/material";
 
 async function CartPage() {
   return (
     <Suspense fallback={<Loading />}>
-      <CartContent />
+      <Container>
+        <CartContent />
+      </Container>
     </Suspense>
   );
 }
--- src/components/cartContent/CartContent.tsx
+++ src/components/cartContent/CartContent.tsx
@@ -1,5 +1,5 @@
 import { getCart } from "@/db/queries/cart";
-import { Container, Box, Stack, Typography, Button } from "@mui/material";
+import { Stack, Typography, Button } from "@mui/material";
 import { log } from "console";
 import { redirect } from "next/navigation";
 import BackToButton from "../backToButton/BackToButton";
@@ -13,29 +13,27 @@ async function CartContent() {
     redirect("/");
   }
   return (
-    <Container maxWidth="sm">
-      <Box>
-        {cart.items &&
-          cart.items.map((cartItem) => (
-            <CartItem item={cartItem} key={cartItem.id} />
-          ))}
-        <hr />
-        <Stack spacing={2}>
-          <Stack direction="row" justifyContent="space-between">
-            <Typography fontWeight="bold">Subtotal</Typography>
-            <Typography fontWeight="bold">{cart.subTotal}</Typography>
-          </Stack>
-          <Typography>Shipping and taxes calculated at checkout</Typography>
-          <Stack>
-            <Button variant="contained">Checkout</Button>
-            <Stack direction="row" spacing={2} justifyContent="center">
-              <Typography variant="caption">or</Typography>
-              <BackToButton target="/product-list" text="Continue Shopping" />
-            </Stack>
+    <>
+      {cart.items &&
+        cart.items.map((cartItem) => (
+          <CartItem item={cartItem} key={cartItem.id} />
+        ))}
+      <hr />
+      <Stack spacing={2}>
+        <Stack direction="row" justifyContent="space-between">
+          <Typography fontWeight="bold">Subtotal</Typography>
+          <Typography fontWeight="bold">{cart.subTotal}</Typography>
+        </Stack>
+        <Typography>Shipping and taxes calculated at checkout</Typography>
+        <Stack>
+          <Button variant="contained">Checkout</Button>
+          <Stack direction="row" spacing={2} justifyContent="center">
+            <Typography variant="caption">or</Typography>
+            <BackToButton target="/product-list" text="Continue Shopping" />
           </Stack>
         </Stack>
-      </Box>
-    </Container>
+      </Stack>
+    </>
   );
 }
 
